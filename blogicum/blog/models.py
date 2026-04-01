64f3087eb0ce856d702a83f89e06f7ba443a7576
Ashymist <Goats4life@yandex.ru>
2026-04-01 20:46:27 +0300

    fix2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,3 +1,122 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Заголовок',
+    )
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        verbose_name='Идентификатор',
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Location(BaseModel):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название места',
+    )
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(BaseModel):
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Заголовок',
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категория',
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Местоположение',
+    )
+    image = models.ImageField(
+        upload_to='posts',
+        blank=True,
+        verbose_name='Изображение',
+    )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Публикация',
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Автор комментария',
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return self.text[:50]
---
+++ blogicum/urls.py
@@ -0,0 +1,4 @@
+"""Compatibility URLConf module for imports as `blogicum.urls`."""
+
+from blogicum.blogicum.urls import *  # noqa: F401,F403
+
